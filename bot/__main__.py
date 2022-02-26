54987d871c63b139d8a47888de9c409cb7497aff
Groosha <groosha@protonmail.com>
2022-02-26 13:57:40 +0300

    Renamed routers

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,15 +1,15 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from bot.handlers.first import r1
-from bot.handlers.second import r2
+from bot.handlers.first import first_router
+from bot.handlers.second import second_router
 
 
 TOKEN = ""
 bot = Bot(TOKEN, parse_mode="HTML")
 dp = Dispatcher()
-dp.include_router(r1)
-dp.include_router(r2)
+dp.include_router(first_router)
+dp.include_router(second_router)
 
 
 async def main() -> None:
--- bot/handlers/second.py
+++ bot/handlers/second.py
@@ -1,9 +1,9 @@
 from aiogram.types import Message
 from aiogram import Router
 
-r2 = Router()
+second_router = Router()
 
 
-@r2.message(commands="test2")
+@second_router.message(commands="test2")
 async def cmd_test2(message: Message):
     await message.answer("This is test 2")
