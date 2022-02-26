59037fb6c9140466ddc504078b6980346d2c2c32
Groosha <groosha@protonmail.com>
2022-02-26 13:56:30 +0300

    Launch with async start_polling, not sync run_polling

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
 from bot.handlers.first import r1
 from bot.handlers.second import r2
@@ -10,9 +12,9 @@ dp.include_router(r1)
 dp.include_router(r2)
 
 
-def main() -> None:
-    dp.run_polling(bot)
+async def main() -> None:
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
